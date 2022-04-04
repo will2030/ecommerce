9f1417fd4eab4d9e7f2b00ce8cc6233d32f99b3e
lucas849 <miguel_lucas_@hotmail.com>
2022-04-04 20:57:07 -0300

    Tela de Login Admin

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
+require_once("functions.php");
 
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
+use Hcode\Model\User;
 
-$app = new Slim();
+$app = new \Slim\Slim();
 
 $app->config('debug', true);
 
@@ -20,9 +19,40 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+	User::verifyLogin();
+
 	$page = new Hcode\PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+    
+	$page = new Hcode\PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login(post('deslogin'), post('despassword'));
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
